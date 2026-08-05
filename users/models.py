5641f61ad3ea988f3b5b1b8c1d8c33386d3f5745
Samir Karki <samirkarki9861975252@gmail.com>
2026-08-05 21:06:02 +0545

    added custom user model registration endpoint

--- users/models.py
+++ users/models.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .managers import UserManager
 
 class User(AbstractUser): 
     username = None #remove username 
     email = models.EmailField(unique=True) #unique customer identity 
 
-    USERNAME_FIELD = "email" #Login using email #django teats as main identity
+    USERNAME_FIELD = "email" #Login using email #django teats as main identity #which filed django must treat as a main login indentifire 
     REQUIRED_FIELDS = [] #NO extra createsuperuser fields
+
+    objects = UserManager()
 
 
 
---
+++ users/serializers.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+User = get_user_model()  
+
+class RegisterSerializer(serializers.ModelSerializer): 
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta: 
+        model = User
+        fields = ["id", "email", "password", "first_name", "last_name"]
+
+    def create(self, validated_data):
+        password = validated_data.pop("password") #removes  password form dictionary, and sotes its value in a seperate variable named password. 
+        return User.objects.create_user(**validated_data)
