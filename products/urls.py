2f35d8f084bf0522fae5b2ed6cce3e61192af7fc
Samir Karki <samirkarki9861975252@gmail.com>
2026-08-11 10:13:20 +0545

    Add product & category detail CRUD endpoints

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import ProductView,CategoryView
+from .views import ProductView, ProductDetailView, CategoryView, CategoryDetailView
 
 
 
 urlpatterns = [
     path("products/",ProductView.as_view(),name="product-list"),
-    path("categories/",CategoryView.as_view(),name="category-list")
+    path("categories/",CategoryView.as_view(),name="category-list"),
+    path( "products/<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
+    path("categories/<int:pk>/",CategoryDetailView.as_view(), name="category-detail")
 ]
+
--- products/views.py
+++ products/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from .models import Product,Category
 from .serializers import CategorySerializer,ProductSerializer
 from .permissions import IsAdminOrReadOnly
+from django.shortcuts import get_object_or_404
 
 
 class ProductView(APIView):
@@ -32,6 +33,7 @@ class CategoryView(APIView):
     def get(self,request): 
         categories = Category.objects.all()
         serializer = CategorySerializer(categories,many=True)
+        serializer.save()
         return Response(serializer.data)
 
     def post(self, request):
@@ -43,3 +45,44 @@ class CategoryView(APIView):
             serializer.data,
             status=status.HTTP_201_CREATED
         )
+    
+class ProductDetailView(APIView):
+    permission_classes = [IsAdminOrReadOnly]
+    def get(self, request, pk):
+        product = get_object_or_404(Product,pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def patch(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(
+         instance= product,
+        data=request.data,
+        partial=True )
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self,request,pk): 
+        product = get_object_or_404(Product,pk=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CategoryDetailView(APIView): 
+    permission_classes = [IsAdminOrReadOnly]
+    def get(self,request,pk): 
+        category = get_object_or_404(Category,pk=pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    def patch(self,request,pk): 
+        category = get_object_or_404(Category,pk=pk)
+        serializer = CategorySerializer(instance=category,data=request.data,partial=True)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data)
+
+    def delete(self,request,pk): 
+        category = get_object_or_404(Category,pk=pk)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
